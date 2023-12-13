e103a41c0f48e872ffc9c358d534fd7d6700ea7a
hamnerlivestock <tanyahamner@yahoo.com>
2023-12-12 17:44:10 -0700

    contact form

---
+++ thankyou.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact Form</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap-3.4.0.css" rel="stylesheet">
+	<style type="text/css">
+	body {
+	background-image: url("Images/Pages Background/lighter leather.jpg");
+	background-repeat: no-repeat;
+}
+	h1 {
+		text-shadow: -1px -1px 1px #fff, 1px 1px 1px #000;
+		color: #9c8468;
+		opacity: 0.3;
+		font-size: 225%;
+	}
+		h1 {
+		color: #C4AA91;
+		padding-left: 5px;
+		opacity: .5;
+		display: inline;
+		padding-bottom: 35px;
+		text-shadow: 2px 2px #000;
+	}
+		p {
+			padding-left: 2%;
+			padding-right: 2%;
+			color: #000000;	
+			
+				
+		}
+
+		h3 {
+			padding-left: 15%;
+			color: #FFFFF9;
+		}
+		
+		form {
+			border: #fffff9;
+		}
+
+		img {
+			float: right;
+			padding-right: 3%;
+}
+		}
+    </style>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<div id="wrapper">
+    	<h1 id="h1nav">Riding for the Brand Photography&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      
+			
+		<a href="contact.php" style="color: #C4AA92">CONTACT TANYA</a>
+  	        
+		</h1> 
+	 	<div id="header">
+			<div class="topnav" id="myTopnav">
+				<a class="navbar-brand" href="index.html"><img src="Images/Pages Background/logoills.png" alt="" width="55" height="22" id="logonav"/></a>
+  				<div class="dropdown">
+    				<button class="dropbtn">MY STORY
+      					<i class="fa fa-caret-down"></i>
+    				</button>
+    				<div class="dropdown-content">
+      					<a href="about.html">ABOUT TANYA</a>
+      					<a href="artiststatement.html">ARTIST STATEMENT</a>
+      					<a href="Resume_CV.pdf" target="_blank">RESUME / CV</a>
+    				</div>
+  				</div> 
+  <div class="dropdown">
+    <button class="dropbtn">PORTFOLIO
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+		 <a href="americancowboy.html">AMERICAN COWBOY</a>
+    	      <a href="rodeo.html">RODEO</a>
+    	         <a href="sports.html">SPORTS</a>
+    	           <a href="racing.html">RACING</a>
+        <a href="https://spark.adobe.com/page/tZoYpFtndJ0aw/" target="_blank">CHI PORTA I SOLDI A CASA</a>
+    </div>
+  </div> 
+  
+  <div class="dropdown">
+    <button class="dropbtn">FINE ART PROJECTS
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+  		 <a href="eyefromthebull.html">EYE FROM THE CONTENTDER</a>
+    	 <a href="fencedin.html">FENCED IN</a>
+    	 <a href="IntotheWoods.html">WOODS OF OUR SOULS</a>
+		 <a href="artofranching.html">Ranching Souls</a>
+		 <a href="artofrodeo.html">Rodeo Chaos</a>
+    </div>
+  </div> 
+	<a href="https://tanyahamner.photoshelter.com/gallery-collection/Wyoming-High-School-Rodeo/C00006CSKrUq_IW4">WYOMING HIGH SCHOOL RODEO</a>
+<a href="workshops.html">WORKSHOPS</a>
+<a href="https://tanyahamner.photoshelter.com/gallery/2022-Calendars/G0000bUwp7_InyHA">CALENDARS</a>
+  	        <div class="dropdown">
+    <button class="dropbtn">PHOTOGRAPHY STORE
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+    <a href="https://tanyahamner.photoshelter.com/gallery-collection/Events/C0000jWJUVTP4NfI">EVENTS</a>
+					 <a href="https://tanyahamner.photoshelter.com/gallery-collection/Rodeo/C0000w6NrULg5jcw">RODEO</a>
+					<a href="https://tanyahamner.photoshelter.com/gallery-collection/Rodeo/C0000w6NrULg5jcw">ARCHIVED RODEO PHOTOS</a>
+					<a href="https://tanyahamner.photoshelter.com/gallery-collection/Wyoming-High-School-Rodeo/C00006CSKrUq_IW4">WYOMING HIGH SCHOOL RODEO</a>
+    	           <a href="https://tanyahamner.photoshelter.com/gallery-collection/Wildlife/C0000eYTqn6RJnws">WILDLIFE</a>
+    	            
+    	          <a href="https://tanyahamner.photoshelter.com/gallery/Still-Life/G0000XfnDgbV2z14/C0000c01MHjZ3nEQ">STILL LIFE</a>
+    	           <a href="https://tanyahamner.photoshelter.com/gallery/Agriculture/G0000wyTXkkAyKwc/C0000c01MHjZ3nEQ">AGRICULTURE</a>
+					<a href="https://tanyahamner.photoshelter.com/gallery-collection/Scenery/C0000PQ.QnFk0t9g">SCENERY</a>
+    </div>
+  </div> 
+            <a href="#">FILM</a>
+            <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+	  </div>
+	</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+  <script src="js/jquery-1.11.3.min.js"></script>
+  
+	<img src="Pages/Contact Page/Me railroad cropped.jpg" alt="" width="50%" class="img-responsive"/>
+
+<h3>Thank You</h3>
+<p style="color: #fffff9">Thank you for contacting me. You should recieve a reply shortly.</p>
+   
+    <!-- Include all compiled plugins (below), or include individual files as needed --> 
+  <script src="js/bootstrap.js"></script>
+  </body>
+</html>
