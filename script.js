035265a1edb53ce0bda0c605805c587173c23cab
hamnerlivestock <tanyahamner@yahoo.com>
2023-12-22 13:57:45 -0700

    changes to photo album

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar Navbar Navbar Navbar  Navbar Navbar Navbar Navbar  Navbar Navbar Navbar Navbar
 document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
@@ -12,4 +13,22 @@ document.addEventListener('click', e => {
         if (dropdown === currentDropdown) return 
         dropdown.classList.remove('active')
     })
+})
+
+// Carousel  Carousel  Carousel  Carousel  Carousel  Carousel  Carousel  Carousel
+const buttons = document.querySelector("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+})
 })
